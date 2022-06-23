27d16e853c7a3efaf73dd13bda06e4767ebc3e4b
Kenneth Geerts <kenneth@sofuto.be>
2022-06-24 00:37:43 +0200

    UI changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,17 +1,23 @@
 class DashboardController < ApplicationController
+  before_action :set_team
+
   def show
-    @team = Team.find_by(code: params[:code])
     @game = @team&.game
 
     redirect_to root_path unless @team
   end
 
   def teams
-    teams = Team.find_by(code: params[:code]).game.teams
-
+    teams = @team.game.teams
     teams = teams.by_current_heart_rate if params[:by] == 'current_heart_rate'
     teams = teams.by_average_heart_rate if params[:by] == 'average_heart_rate'
 
     render partial: 'teams', locals: { teams: teams }
+  end
+
+  private
+
+  def set_team
+    @team = Team.find_by(code: params[:code])
   end
 end
--- app/javascript/controllers/live_heartbeat_controller.js
+++ app/javascript/controllers/live_heartbeat_controller.js
@@ -1,24 +1,15 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'heartRate', 'measuredAt', 'heartEmoji' ]
+  static targets = [ 'heartRate', 'heartEmoji' ]
   static values = { team: Object }
 
   connect() {
     const socket = new WebSocket(this.teamValue.ws_url)
     socket.onmessage = (event) => {
       const message = JSON.parse(event.data)
-      console.log(message)
-      this.updateTargets(message)
+      this.heartRateTarget.textContent = message.data.heart_rate
+      this.heartEmojiTarget.textContent = heartRate > 0 ? '❤️' : '🤍'
     }
-  }
-
-  updateTargets(message) {
-    const heartRate = message.data.heart_rate
-    const measuredAt = new Date(message.measured_at)?.toLocaleTimeString()
-
-    this.heartRateTarget.textContent = heartRate
-    this.measuredAtTarget.textContent = measuredAt
-    this.heartEmojiTarget.textContent = heartRate > 0 ? '❤️' : '🤍'
   }
 }
