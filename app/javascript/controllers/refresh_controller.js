51cccb3cd2c16376699187a2cbaba3bd4c7c3b4d
Kenneth Geerts <kenneth@sofuto.be>
2022-06-23 23:33:07 +0200

    Refresh every 2s

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -2,7 +2,7 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   connect() {
-    this.timeout = setInterval(() => { this.element.reload() }, 5000)
+    this.timeout = setInterval(() => { this.element.reload() }, 2000)
   }
 
   disconnect() {
